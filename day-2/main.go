a32bfa684cd2b8c3d255f23daa269b430b98f304
weizhang9 <wei.zhang@sky.uk>
2019-12-03 21:29:12 +0000

    refactor

--- day-2/main.go
+++ day-2/main.go
@@ -53,26 +53,21 @@ func getResultFromParams(nums []int, param1 int, param2 int) {
 	nums[1], nums[2] = param1, param2
 
 	for p := range nums {
-
-		if p%4 == 0 && p <= len(nums)-4 {
-			formulaRewrite(nums, p)
-		}
+		rewriteFromFormula(nums, p)
 	}
 	fmt.Println("Day 2 Part One result", nums[0])
 }
 
 func getParamsFromResult(slice []int, result int) {
 	nums := make([]int, len(slice))
-	
+
 	for param1 := 0; param1 < 100; param1++ {
 		for param2 := 0; param2 < 100; param2++ {
 			copy(nums, slice)
 			nums[1], nums[2] = param1, param2
-			p := 0
 
-			for p < len(nums)-4 {
-				formulaRewrite(nums, p)
-				p += 4
+			for p := range nums {
+				rewriteFromFormula(nums, p)
 			}
 
 			if nums[0] == result {
@@ -86,13 +81,17 @@ func getParamsFromResult(slice []int, result int) {
 
 }
 
-func formulaRewrite(nums []int, pos int) {
-	switch nums[pos] {
-	case 1:
-		nums[nums[pos+3]] = nums[nums[pos+1]] + nums[nums[pos+2]]
-	case 2:
-		nums[nums[pos+3]] = nums[nums[pos+1]] * nums[nums[pos+2]]
-	case 99:
-		break
+func rewriteFromFormula(nums []int, pos int) {
+
+	if pos%4 == 0 && pos <= len(nums)-4 {
+		switch nums[pos] {
+		case 1:
+			nums[nums[pos+3]] = nums[nums[pos+1]] + nums[nums[pos+2]]
+		case 2:
+			nums[nums[pos+3]] = nums[nums[pos+1]] * nums[nums[pos+2]]
+		case 99:
+			break
+		}
 	}
+
 }
